977b7f9638217e4e07a18f7a4df001e7371040ee
Avdesh kumar <ku.kumar.avdesh786@gmail.com>
2023-12-17 22:32:56 +0530

    connected io socekt

---
+++ client/src/screens/lobby.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const LobbyScreen=()=>{
+
+    return(
+        <div>
+            <h1>Lobby Screen</h1>
+        </div>
+    );
+
+}
+
+export default LobbyScreen;
